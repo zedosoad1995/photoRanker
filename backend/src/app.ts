a8262a06f6cd5410a915cbea153dc856c5889f77
zedosoad1995 <joao.pires.queiroz@gmail.com>
2023-06-16 23:32:20 +0100

    Add cookies middleware

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,5 +1,6 @@
 import express from "express";
 import "express-async-errors";
+import cookieSession from "cookie-session";
 import { json } from "body-parser";
 import { NotFoundError } from "@/errors/NotFoundError";
 import { errorHandler } from "@/middlewares/errorHandler";
@@ -7,6 +8,12 @@ import routes from "@/routes";
 
 const app = express();
 app.use(json());
+app.use(
+  cookieSession({
+    signed: false,
+    secure: false,
+  })
+);
 app.use(routes);
 
 app.all("*", async (req, res) => {
