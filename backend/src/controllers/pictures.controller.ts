c8bf92a1180fb76098147f5dac5725dbe900e9a4
zedosoad1995 <joao.pires.queiroz@gmail.com>
2023-06-19 22:50:11 +0100

    Add delete picture endpoint

--- backend/src/controllers/pictures.controller.ts
+++ backend/src/controllers/pictures.controller.ts
@@ -1,6 +1,9 @@
 import { ELO_INIT } from "@/constants/picture";
 import { BadRequestError } from "@/errors/BadRequestError";
+import { ForbiddenError } from "@/errors/ForbiddenError";
+import { isRegular } from "@/helpers/role";
 import { PictureModel } from "@/models/picture";
+import { User } from "@prisma/client";
 import { Request, Response } from "express";
 
 export const upload = async (req: Request, res: Response) => {
@@ -23,4 +26,31 @@ export const upload = async (req: Request, res: Response) => {
   res.status(200).json({
     message: "Image successfully uploaded",
   });
+};
+
+export const deleteOne = async (req: Request, res: Response) => {
+  const pictureId = req.params.pictureId;
+  const loggedUser = req.loggedUser as User;
+
+  const existingPicture = await PictureModel.findUnique({
+    where: {
+      id: pictureId,
+    },
+  });
+
+  if (!existingPicture) {
+    throw new BadRequestError("Picture does not exist");
+  }
+
+  if (isRegular(loggedUser.role) && existingPicture.userId !== loggedUser.id) {
+    throw new ForbiddenError("User cannot delete this picture");
+  }
+
+  await PictureModel.delete({
+    where: {
+      id: pictureId,
+    },
+  });
+
+  res.sendStatus(204);
 };
--- backend/src/routes/pictures.route.ts
+++ backend/src/routes/pictures.route.ts
@@ -1,10 +1,11 @@
 import { Router } from "express";
-import { upload } from "@/controllers/pictures.controller";
+import { deleteOne, upload } from "@/controllers/pictures.controller";
 import { checkAuth } from "@/middlewares/checkAuth";
 import { storeImage } from "@/middlewares/storeImage";
 
 const router = Router();
 
 router.post("/", checkAuth, storeImage, upload);
+router.delete("/", checkAuth, deleteOne);
 
 export default router;
