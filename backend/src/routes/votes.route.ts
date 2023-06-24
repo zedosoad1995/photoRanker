5cfd98812316f64ae61efb2cd0318739cc0ebc79
zedosoad1995 <joao.pires.queiroz@gmail.com>
2023-06-24 12:26:08 +0100

    Add form validation to vote endpoint

--- backend/src/routes/votes.route.ts
+++ backend/src/routes/votes.route.ts
@@ -1,9 +1,11 @@
 import { Router } from "express";
 import { vote } from "@/controllers/votes.controller";
 import { checkAuth } from "@/middlewares/checkAuth";
+import { voteSchema } from "@/schemas/vote/vote";
+import { validateForm } from "@/middlewares/validateForm";
 
 const router = Router();
 
-router.post("/", checkAuth, vote);
+router.post("/", checkAuth, validateForm(voteSchema), vote);
 
 export default router;
