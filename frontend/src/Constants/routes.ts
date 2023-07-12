5a8b4e52b5c16c34d2aaab50d6354e7e7f0174e4
zedosoad1995 <joao.pires.queiroz@gmail.com>
2023-07-12 23:29:49 +0100

    Create basic Vote page

--- frontend/src/Constants/routes.ts
+++ frontend/src/Constants/routes.ts
@@ -1,6 +1,7 @@
 export const HOME = "/";
 export const LOGIN = "/login";
 export const REGISTER = "/register";
+export const VOTE = "/vote";
 export const SETTINGS = "/settings";
 
 export const NON_PROTECTED_ROUTES = [LOGIN, REGISTER];
