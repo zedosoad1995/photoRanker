2579f168250c1c6d836cc5aaf62bdb88abab80d4
zedosoad1995 <joao.pires.queiroz@gmail.com>
2023-07-19 00:14:22 +0100

    Add add photo button and improve my photos styling to have cards

--- frontend/src/Components/Button.tsx
+++ frontend/src/Components/Button.tsx
@@ -3,15 +3,22 @@ interface IButton {
   type?: "button" | "reset" | "submit";
   onClick?: React.MouseEventHandler<HTMLButtonElement>;
   size?: "medium" | "large";
+  isFull?: boolean;
 }
 
-export default function Button({ children, type, onClick, size = "medium" }: IButton) {
+export default function Button({
+  children,
+  type,
+  onClick,
+  size = "medium",
+  isFull = true,
+}: IButton) {
   return (
     <button
       type={type}
-      className={`flex w-full justify-center rounded-md bg-primary font-semibold leading-6 text-white shadow-sm hover:bg-primary-hover focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-primary ${
+      className={`flex justify-center rounded-md bg-primary font-semibold leading-6 text-white shadow-sm hover:bg-primary-hover focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-primary ${
         size === "medium" ? "px-3 py-1.5 text-sm" : "px-5 py-2.5 text-base"
-      }`}
+      } ${isFull ? "w-fill" : ""}`}
       onClick={onClick}
     >
       {children}
--- frontend/src/Pages/MyPhotos.tsx
+++ frontend/src/Pages/MyPhotos.tsx
@@ -1,21 +1,29 @@
+import Button from "@/Components/Button";
 import { getImage, getManyPictures } from "@/Services/picture";
 import { useEffect, useState } from "react";
+import { IPicture } from "../../../backend/src/types/picture";
 
 export default function MyPhotos() {
   const [pics, setPics] = useState<string[]>([]);
+  const [picsInfo, setPicsInfo] = useState<IPicture[]>([]);
 
   useEffect(() => {
     getManyPictures().then(async (res) => {
       const pics: string[] = [];
+      const picsInfo: IPicture[] = [];
       for (const pic of res.pictures) {
         try {
           const tempPic = await getImage(pic.filepath);
           pics.push(URL.createObjectURL(tempPic));
+          picsInfo.push(pic);
         } catch (error) {}
       }
       setPics(pics);
+      setPicsInfo(picsInfo);
     });
+  }, []);
 
+  useEffect(() => {
     return () => {
       pics.forEach((pic) => {
         URL.revokeObjectURL(pic);
@@ -25,9 +33,23 @@ export default function MyPhotos() {
 
   return (
     <div className="flow-root pb-4 md:pb-12 w-full md:w-[650px] lg:w-[900px] xl:w-[1150px] mx-auto">
-      {pics.map((pic) => (
-        <img className="w-1/2 md:w-1/3 lg:w-1/4 p-3 float-left" src={pic} />
-      ))}
+      <Button>
+        <span className="mr-3 text-xl !leading-5">+</span>
+        <span>Add Photo</span>
+      </Button>
+      <div className="-mx-3">
+        {pics.map((pic, index) => (
+          <div key={pic} className="w-1/2 md:w-1/3 lg:w-1/4 float-left p-3">
+            <div className="cursor-pointer shadow-md rounded-md overflow-hidden">
+              <img src={pic} alt={`picture-${index}`} />
+              <div className="p-3 font-semibold text-sm">
+                <div>elo: {picsInfo[index].elo}</div>
+                <div>votes: {picsInfo[index].numVotes}</div>
+              </div>
+            </div>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
