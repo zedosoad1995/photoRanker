1a4652dc72a82ade9a39641cacc66cd6bb59b856
zedosoad1995 <joao.pires.queiroz@gmail.com>
2023-07-24 00:27:06 +0100

    Change sex to gender

--- frontend/src/Constants/user.ts
+++ frontend/src/Constants/user.ts
@@ -2,8 +2,3 @@ export const AGE_OPTIONS = [
   ...Array.from({ length: 100 - 18 }, (_, index) => (index + 18).toString()),
   "100+",
 ];
-
-export enum SEXES {
-  MALE = "Male",
-  FEMALE = "Female",
-}
