62006a1b3aeac8f216a12b2c26ba1ea0e1477243
zedosoad1995 <joao.pires.queiroz@gmail.com>
2023-07-25 02:33:42 +0100

    Add placeholder when there are no photos

--- frontend/src/Pages/MyPhotos/MyPhotos.tsx
+++ frontend/src/Pages/MyPhotos/MyPhotos.tsx
@@ -5,7 +5,7 @@ import { IPicture } from "../../../../backend/src/types/picture";
 import { LIMIT_PICTURES, MIN_HEIGHT, MIN_WIDTH } from "../../../../backend/src/constants/picture";
 import UploadPhotoModal from "./UploadPhotoModal";
 import { getImageDimensionsFromBase64 } from "@/Utils/image";
-import { XMarkIcon } from "@heroicons/react/20/solid";
+import { ArrowUpTrayIcon, XMarkIcon } from "@heroicons/react/20/solid";
 import DeletePhotoModal from "./DeletePhotoModal";
 import { getLoggedUser } from "@/Utils/user";
 
@@ -17,6 +17,7 @@ export default function MyPhotos() {
   const [pics, setPics] = useState<string[]>([]);
   const [picsInfo, setPicsInfo] = useState<IPicture[]>([]);
   const [isOpen, setIsOpen] = useState(false);
+  const [isLoading, setIsLoading] = useState(true);
   const [isOpenDelete, setIsOpenDelete] = useState(false);
   const [picToDeleteIndex, setPicToDeleteIndex] = useState<number | null>(null);
 
@@ -24,19 +25,23 @@ export default function MyPhotos() {
   const hasReachedPicsLimit = pics.length >= LIMIT_PICTURES && loggedUser?.role == "REGULAR";
 
   const getPictures = () => {
-    return getManyPictures(loggedUser?.id).then(async (res) => {
-      const pics: string[] = [];
-      const picsInfo: IPicture[] = [];
-      for (const pic of res.pictures) {
-        try {
-          const tempPic = await getImage(pic.filepath);
-          pics.push(URL.createObjectURL(tempPic));
-          picsInfo.push(pic);
-        } catch (error) {}
-      }
-      setPics(pics);
-      setPicsInfo(picsInfo);
-    });
+    return getManyPictures(loggedUser?.id)
+      .then(async (res) => {
+        const pics: string[] = [];
+        const picsInfo: IPicture[] = [];
+        for (const pic of res.pictures) {
+          try {
+            const tempPic = await getImage(pic.filepath);
+            pics.push(URL.createObjectURL(tempPic));
+            picsInfo.push(pic);
+          } catch (error) {}
+        }
+        setPics(pics);
+        setPicsInfo(picsInfo);
+      })
+      .finally(() => {
+        setIsLoading(false);
+      });
   };
 
   useEffect(() => {
@@ -106,6 +111,31 @@ export default function MyPhotos() {
     setIsOpenDelete(false);
   };
 
+  const EmptyPlaceholder = () => {
+    return (
+      <div className="flex flex-col items-center">
+        <ArrowUpTrayIcon className="w-28 h-28" />
+        <div className="text-xl font-bold mt-4">Upload Photo</div>
+        <div className="mt-2 mb-8 text-center">
+          You currently have no photos.
+          <br />
+          Add you first photo to start receiving votes!
+        </div>
+        <input
+          type="file"
+          accept=".jpg, .jpeg, .png"
+          ref={fileInputRef}
+          onChange={handleFileChange}
+          className="hidden"
+        />
+        <Button onClick={handleFileSelect} isFull={false}>
+          <span className="mr-3 text-xl !leading-5">+</span>
+          <span>Add Photo</span>
+        </Button>
+      </div>
+    );
+  };
+
   return (
     <>
       <DeletePhotoModal
@@ -123,36 +153,41 @@ export default function MyPhotos() {
         }}
       />
       <div className="flow-root pb-4 md:pb-12 w-full md:w-[650px] lg:w-[900px] xl:w-[1150px] mx-auto">
-        <input
-          type="file"
-          accept="image/*"
-          ref={fileInputRef}
-          onChange={handleFileChange}
-          className="hidden"
-        />
-        <Button disabled={hasReachedPicsLimit} onClick={handleFileSelect} isFull={false}>
-          <span className="mr-3 text-xl !leading-5">+</span>
-          <span>Add Photo</span>
-        </Button>
-        <div className="-mx-3">
-          {pics.map((pic, index) => (
-            <div key={pic} className="w-1/2 md:w-1/3 lg:w-1/4 float-left p-3">
-              <div className="cursor-pointer shadow-md rounded-md overflow-hidden">
-                <div className="relative">
-                  <XMarkIcon
-                    onClick={handleClickDeletePic(index)}
-                    className="absolute right-[2%] top-[2%] origin-top-right h-5 w-5 cursor-pointer rounded-full bg-white bg-opacity-30 hover:bg-opacity-60 transition duration-200"
-                  />
-                  <img className="mx-auto w-full" src={pic} alt={`picture-${index}`} />
+        {!isLoading && pics.length === 0 && <EmptyPlaceholder />}
+        {!isLoading && pics.length > 0 && (
+          <>
+            <input
+              type="file"
+              accept="image/*"
+              ref={fileInputRef}
+              onChange={handleFileChange}
+              className="hidden"
+            />
+            <Button disabled={hasReachedPicsLimit} onClick={handleFileSelect} isFull={false}>
+              <span className="mr-3 text-xl !leading-5">+</span>
+              <span>Add Photo</span>
+            </Button>
+            <div className="-mx-3">
+              {pics.map((pic, index) => (
+                <div key={pic} className="w-1/2 md:w-1/3 lg:w-1/4 float-left p-3">
+                  <div className="cursor-pointer shadow-md rounded-md overflow-hidden">
+                    <div className="relative">
+                      <XMarkIcon
+                        onClick={handleClickDeletePic(index)}
+                        className="absolute right-[2%] top-[2%] origin-top-right h-5 w-5 cursor-pointer rounded-full bg-white bg-opacity-30 hover:bg-opacity-60 transition duration-200"
+                      />
+                      <img className="mx-auto w-full" src={pic} alt={`picture-${index}`} />
+                    </div>
+                    <div className="p-3 font-semibold text-sm">
+                      <div>elo: {picsInfo[index].elo}</div>
+                      <div>votes: {picsInfo[index].numVotes}</div>
+                    </div>
+                  </div>
                 </div>
-                <div className="p-3 font-semibold text-sm">
-                  <div>elo: {picsInfo[index].elo}</div>
-                  <div>votes: {picsInfo[index].numVotes}</div>
-                </div>
-              </div>
+              ))}
             </div>
-          ))}
-        </div>
+          </>
+        )}
       </div>
     </>
   );
