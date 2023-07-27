eb0cf7ed3c629b403cedce1d1b1a1be319124d93
zedosoad1995 <joao.pires.queiroz@gmail.com>
2023-07-27 16:50:09 +0100

    Add google login endpoint

--- backend/src/controllers/auth.controller.ts
+++ backend/src/controllers/auth.controller.ts
@@ -3,12 +3,13 @@ import { UserModel } from "@/models/user";
 import jwt from "jsonwebtoken";
 import { Request, Response } from "express";
 import { UnauthorizedError } from "@/errors/UnauthorizedError";
+import { OAuth2Client } from "google-auth-library";
 
 export const signIn = async (req: Request, res: Response) => {
   const { email, password } = req.body;
 
   const user = await UserModel.findUnique({ where: { email } });
-  if (!user) {
+  if (!user || !user.password) {
     throw new UnauthorizedError("Invalid credentials");
   }
 
@@ -37,4 +38,46 @@ export const signOut = (req: Request, res: Response) => {
   res.clearCookie("session");
 
   res.status(204).send();
+};
+
+const oAuth2Client = new OAuth2Client(
+  process.env.GOOGLE_CLIENT_ID,
+  process.env.GOOGLE_CLIENT_SECRET,
+  "postmessage"
+);
+
+export const signInGoogle = async (req: Request, res: Response) => {
+  const { tokens } = await oAuth2Client.getToken(req.body.code);
+
+  if (!tokens.access_token) {
+    throw new UnauthorizedError("Google access token is undefined");
+  }
+
+  const { email, sub: googleId } = await oAuth2Client.getTokenInfo(tokens.access_token);
+
+  const user = await UserModel.findFirst({
+    where: {
+      email,
+      googleId,
+    },
+  });
+
+  if (!user) {
+    throw new UnauthorizedError("User does not exist. Invalid email or googleId");
+  }
+
+  const userJwt = jwt.sign(
+    {
+      email: user.email,
+    },
+    process.env.JWT_KEY!
+  );
+
+  res.cookie("session", {
+    jwt: userJwt,
+  });
+
+  const userNoPassword = UserModel.exclude(user, ["password"]);
+
+  res.status(200).json({ user: userNoPassword });
 };
--- backend/src/routes/auth.route.ts
+++ backend/src/routes/auth.route.ts
@@ -1,11 +1,12 @@
 import { Router } from "express";
-import { signIn, signOut } from "@/controllers/auth.controller";
+import { signIn, signInGoogle, signOut } from "@/controllers/auth.controller";
 import { validateForm } from "@/middlewares/validateForm";
 import { signInSchema } from "@/schemas/auth/signIn";
 
 const router = Router();
 
 router.post("/login", validateForm(signInSchema), signIn);
+router.post("/login/google", signInGoogle);
 router.post("/logout", signOut);
 
 export default router;
