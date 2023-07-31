108539f24738ac29488318a74b2a8bd9b7c0c542
João Queiroz <joao.pires.queiroz@gmail.com>
2023-07-31 23:45:52 +0100

    Create link to go back to home page

--- frontend/src/Pages/ExpiredValidation.tsx
+++ frontend/src/Pages/ExpiredValidation.tsx
@@ -1,14 +1,19 @@
+import { HOME } from "@/Constants/routes";
+
 export default function ExpiredValidation() {
   return (
     <div className="p-4 md:p-12 max-w-5xl mx-auto">
       <h3 className="text-xl font-semibold mb-4">
         Verification Link Invalid or Expired
       </h3>
-      <div>
+      <div className="mb-4">
         We're sorry, but the verification link you've clicked is invalid or has
         expired. This can happen if you've already verified your account, the
         link was only meant for a single-use, or it has expired after a certain
         period.
+      </div>
+      <div>
+        <a href={HOME}>Click here</a> to go back to the home page.
       </div>
     </div>
   );
