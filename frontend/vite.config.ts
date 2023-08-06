762f29ba4fc1cc10a88454f2105a23d98a7d9c4d
João Queiroz <joao.pires.queiroz@gmail.com>
2023-08-06 15:15:16 +0100

    Create VITE env var

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,15 +1,19 @@
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import { fileURLToPath, URL } from "url";
 import mkcert from "vite-plugin-mkcert";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  server: {
-    port: 3000,
-  },
-  plugins: [react(), mkcert()],
-  resolve: {
-    alias: [{ find: "@", replacement: fileURLToPath(new URL("./src", import.meta.url)) }],
-  },
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), "");
+
+  return {
+    server: {
+      port: 3000,
+    },
+    plugins: [react(), ...(env.VITE_ENV ? [mkcert()] : [])],
+    resolve: {
+      alias: [{ find: "@", replacement: fileURLToPath(new URL("./src", import.meta.url)) }],
+    },
+  };
 });
