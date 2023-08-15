a6d07b4d039f1b81e5291fd38eedbd66e4f2e349
João Queiroz <joao.pires.queiroz@gmail.com>
2023-08-15 15:27:41 +0100

    Fix tests

--- backend/src/__mocks__/container.ts
+++ backend/src/__mocks__/container.ts
@@ -1,5 +1,11 @@
+import { Elo } from "@/repositories/rating/elo";
+import { Glicko2 } from "@/repositories/rating/glicko2";
+
 export const mainStorageInteractor = {
   saveNewImage: jest.fn(),
   getImageUrl: jest.fn(),
   deleteImage: jest.fn(),
 };
+
+export const elo = new Elo();
+export const glicko2 = new Glicko2();
