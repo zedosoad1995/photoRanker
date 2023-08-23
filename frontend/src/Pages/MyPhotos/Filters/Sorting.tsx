9ed90d86c89f6dc29dd52bf7acdcbba55327f953
João Queiroz <joao.pires.queiroz@gmail.com>
2023-08-23 01:36:05 +0100

    Reverse newest oldest order

--- frontend/src/Pages/MyPhotos/Filters/Sorting.tsx
+++ frontend/src/Pages/MyPhotos/Filters/Sorting.tsx
@@ -16,12 +16,12 @@ const Sorting = ({ isAdmin, option, onChange }: ISorting) => {
           { id: "score asc", label: "Score Lowest to Highest" },
           { id: "numVotes desc", label: "Votes Highest to Lowest" },
           { id: "numVotes asc", label: "Votes Lowest to Highest" },
-          { id: "createdAt desc", label: "Creation Date Oldest to Newest" },
-          { id: "createdAt asc", label: "Creation Date Newest to Oldest" },
+          { id: "createdAt desc", label: "Creation Date Newest to Oldest" },
+          { id: "createdAt asc", label: "Creation Date Oldest to Newest" },
           ...(isAdmin
             ? [
-                { id: "reportedDate desc", label: "Reported Date Oldest to Newest" },
-                { id: "reportedDate asc", label: "Reported Date Newest to Oldest" },
+                { id: "reportedDate desc", label: "Reported Date Newest to Oldest" },
+                { id: "reportedDate asc", label: "Reported Date Oldest to Newest" },
               ]
             : []),
         ]}
