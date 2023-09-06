20b9359e5ee12cac0fa2cfb0bac17f166d33400b
João Queiroz <joao.pires.queiroz@gmail.com>
2023-09-07 00:48:11 +0100

    Update email html location

--- backend/src/helpers/mail.ts
+++ backend/src/helpers/mail.ts
@@ -3,7 +3,7 @@ import path from "path";
 import { VIEWS_FOLDER_PATH } from "@/constants/email";
 
 export const getEmailHtml = async (emailPath: string, data: { [name: string]: any }) => {
-  const templatePath = path.resolve(process.cwd(), VIEWS_FOLDER_PATH, emailPath);
+  const templatePath = path.resolve("..", VIEWS_FOLDER_PATH, emailPath);
 
   return ejs.renderFile(templatePath, data);
 };
