91db3b0d0bd28ac8cb978116d2a381bdc35c94bf
João Queiroz <joao.pires.queiroz@gmail.com>
2023-09-07 02:15:28 +0100

    Add backend path

---
+++ backend/src/constants/path.ts
@@ -0,0 +1,3 @@
+import path from "path";
+
+export const BACKEND_PATH = path.resolve(__dirname.split(path.sep + "backend")[0], "backend");
--- backend/src/helpers/mail.ts
+++ backend/src/helpers/mail.ts
@@ -1,13 +1,10 @@
 import ejs from "ejs";
 import path from "path";
 import { VIEWS_FOLDER_PATH } from "@/constants/email";
-
-const basePath = __dirname.includes(path.sep + "backend")
-  ? __dirname.split(path.sep + "backend")[0]
-  : __dirname;
+import { BACKEND_PATH } from "@/constants/path";
 
 export const getEmailHtml = async (emailPath: string, data: { [name: string]: any }) => {
-  const templatePath = path.resolve(basePath, "backend", VIEWS_FOLDER_PATH, emailPath);
+  const templatePath = path.resolve(BACKEND_PATH, VIEWS_FOLDER_PATH, emailPath);
 
   return ejs.renderFile(templatePath, data);
 };
