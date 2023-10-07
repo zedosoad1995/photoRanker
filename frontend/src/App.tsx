cb60a0bfc3517e2642b71f8d1a2e5d554bb502ac
João Queiroz <joao.pires.queiroz@gmail.com>
2023-10-07 19:48:18 +0100

    Make how it works section responsive

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -230,110 +230,121 @@ function App() {
               </button>
             </div>
           </div>
-          {/* <div className="hidden xl:flex h-full items-center gap-6">
-            <div className="w-[60%]">
-              <div className="font-semibold text-white text-[68px] mb-[20px]">
-                Find your best look
-              </div>
-              <div className="text-white/70 text-[24px] mb-[40px]">
-                Let strangers rate your pictures, in a fun side-by-side comparison
-              </div>
-              <button className="bg-[#a52a2a] hover:bg-[#7c2020] transition-colors ease-linear duration-300 text-[22px] leading-[65px] font-medium text-white rounded-[60px] px-[45px]">
-                Test your photo now
-              </button>
-            </div>
-            <div className="flex justify-center gap-5">
-              <div className="cursor-pointer rounded-t-md shadow-md w-56">
-                <div className="relative">
-                  <div className="rounded-t-md overflow-hidden">
-                    <img className="mx-auto w-full" src="/dude.jpg" alt="hero-pic" />
-                  </div>
-                </div>
-                <div className="p-3 font-semibold text-[8px] min-[300px]:text-[10px] min-[350px]:text-xs xs:text-sm bg-white rounded-b-md">
-                  <div className="flex justify-between">
-                    <span>Score:</span> <span>Top 17%</span>
-                  </div>
-                  <hr className="my-2" />
-                  <div className="flex justify-between">
-                    <span>Votes:</span> <span>35</span>
-                  </div>
-                </div>
-              </div>
-              <div className="cursor-pointer rounded-t-md shadow-md w-56">
-                <div className="relative">
-                  <div className="rounded-t-md overflow-hidden">
-                    <img className="mx-auto w-full" src="/dude2.jpg" alt="hero-pic" />
-                  </div>
-                </div>
-                <div className="p-3 font-semibold text-[8px] min-[300px]:text-[10px] min-[350px]:text-xs xs:text-sm bg-white rounded-b-md">
-                  <div className="flex justify-between">
-                    <span>Score:</span> <span>Top 22%</span>
-                  </div>
-                  <hr className="my-2" />
-                  <div className="flex justify-between">
-                    <span>Votes:</span> <span>29</span>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div> */}
         </div>
       </section>
-      {/* <section className="pt-[104px] pb-[95px] px-[70px]">
-        <div className="text-[48px] leading-[29px] text-[#374048] font-semibold text-center mb-[75px]">
+      <section className="pt-20 md:pt-[104px] pb-[95px] px-5 sm:px-[70px]">
+        <div className="text-3xl md:text-[48px] leading-[29px] text-[#374048] font-semibold text-center mb-[75px]">
           HOW IT WORKS
         </div>
-        <div className="flex gap-5">
-          <div className="w-1/2 bg-[#B2F7D5] rounded-3xl py-8 px-[15%]">
+        <div className="flex md:hidden gap-5 flex-col">
+          <div className="bg-[#B2F7D5] rounded-3xl py-8 px-[15%] m-auto">
             <div className="rounded-lg overflow-hidden">
               <img src="/upload.png" alt="step1" />
             </div>
           </div>
-          <div className="w-1/2 pl-20 my-auto">
-            <div className="text-[#374048] text-4xl font-semibold mb-5">1. Upload Photo</div>
-            <div className="text-[#82898f] text-lg font-light mb-5">
+          <div className="pl-3 lg:pl-20 my-auto">
+            <div className="text-[#374048] text-2xl font-semibold mb-5">1. Upload Photo</div>
+            <div className="text-[#82898f] text-base font-light mb-5">
               Upload a picture you want to get rated. You can upload multiple if you wish.
             </div>
-            <button className="bg-[#0084FF] hover:bg-[#006ACC] transition-colors ease-linear duration-300 text-[22px] leading-[65px] font-medium text-white rounded-[60px] px-[45px]">
-              Upload photo
+            <button className="bg-[#0084FF] hover:bg-[#006ACC] transition-colors ease-linear duration-300 text-base py-4 px-8 font-medium text-white rounded-[60px]">
+              Upload Photo
             </button>
           </div>
         </div>
-        <div className="flex gap-5 mt-[100px]">
-          <div className="w-1/2 pr-20 my-auto">
-            <div className="text-[#374048] text-4xl font-semibold mb-5">2. Receive Votes</div>
+        <div className="hidden md:flex gap-5 ">
+          <div className="w-1/2 bg-[#B2F7D5] rounded-3xl py-8 px-[15%] m-auto">
+            <div className="rounded-lg overflow-hidden">
+              <img src="/upload.png" alt="step1" />
+            </div>
+          </div>
+          <div className="w-1/2 pl-3 lg:pl-20 my-auto">
+            <div className="text-[#374048] text-3xl lg:text-4xl font-semibold mb-5">
+              1. Upload Photo
+            </div>
+            <div className="text-[#82898f] text-lg font-light mb-5">
+              Upload a picture you want to get rated. You can upload multiple if you wish.
+            </div>
+            <button className="bg-[#0084FF] hover:bg-[#006ACC] transition-colors ease-linear duration-300 max-[500px]:text-lg max-[500px]:py-4 max-[500px]:px-10 text-lg py-[18px] px-[34px] font-medium text-white rounded-[60px]">
+              Upload Photo
+            </button>
+          </div>
+        </div>
+
+        <div className="flex md:hidden gap-5 flex-col mt-20">
+          <div className="bg-[#B2E6F7] rounded-3xl py-8 px-[15%] m-auto">
+            <div className="rounded-lg overflow-hidden">
+              <img src="/side-by-side-click.png" alt="step2" />
+            </div>
+          </div>
+          <div className="pl-3 lg:pl-20 my-auto">
+            <div className="text-[#374048] text-2xl font-semibold mb-5">2. Receive Votes</div>
+            <div className="text-[#82898f] text-base font-light mb-5">
+              Get rated by strangers based on attractiveness. Our unique side-by-side voting system
+              allows for more votes and avoids gray areas typical in 1-10 rating.
+            </div>
+            <button className="bg-[#0084FF] hover:bg-[#006ACC] transition-colors ease-linear duration-300 text-base py-4 px-8 font-medium text-white rounded-[60px]">
+              Start Voting Now
+            </button>
+          </div>
+        </div>
+
+        <div className="hidden md:flex gap-5 mt-[100px]">
+          <div className="w-1/2 pr-3 lg:pr-20 my-auto">
+            <div className="text-[#374048] text-3xl lg:text-4xl font-semibold mb-5">
+              2. Receive Votes
+            </div>
             <div className="text-[#82898f] text-lg font-light mb-5">
               Get rated by strangers based on attractiveness. Our unique side-by-side voting system
               allows for more votes and avoids gray areas typical in 1-10 rating.
             </div>
-            <button className="bg-[#0084FF] hover:bg-[#006ACC] transition-colors ease-linear duration-300 text-[22px] leading-[65px] font-medium text-white rounded-[60px] px-[45px]">
+            <button className="bg-[#0084FF] hover:bg-[#006ACC] transition-colors ease-linear duration-300 max-[500px]:text-lg max-[500px]:py-4 max-[500px]:px-10 text-lg py-[18px] px-[34px] font-medium text-white rounded-[60px]">
               Start Voting Now
             </button>
           </div>
-          <div className="w-1/2 bg-[#82E6F7] rounded-3xl p-8">
-            <div className="rounded-t-lg overflow-hidden">
+          <div className="w-1/2 bg-[#B2E6F7] rounded-3xl p-8 m-auto">
+            <div className="rounded-lg overflow-hidden">
               <img src="/side-by-side-click.png" alt="step2" />
             </div>
           </div>
         </div>
-        <div className="flex gap-5 mt-[100px]">
-          <div className="w-1/2 bg-[#FFFFB2] rounded-3xl px-[9%] pt-8">
+
+        <div className="flex md:hidden gap-5 flex-col mt-20">
+          <div className="bg-[#FFFFB2] rounded-3xl py-8 px-[15%] m-auto">
+            <div className="rounded-lg overflow-hidden">
+              <img src="/score.png" alt="step3" />
+            </div>
+          </div>
+          <div className="pl-3 lg:pl-20 my-auto">
+            <div className="text-[#374048] text-2xl font-semibold mb-5">3. Get Score</div>
+            <div className="text-[#82898f] text-base font-light mb-5">
+              Discover how does you photo compare to the general population.
+            </div>
+            <button className="bg-[#0084FF] hover:bg-[#006ACC] transition-colors ease-linear duration-300 text-base py-4 px-8 font-medium text-white rounded-[60px]">
+              Find your rating
+            </button>
+          </div>
+        </div>
+        <div className="hidden md:flex gap-5 mt-[100px]">
+          <div className="w-1/2 bg-[#FFFFB2] rounded-3xl px-[9%] pt-8 m-auto">
             <div className="rounded-t-lg overflow-hidden">
               <img src="/score.png" alt="step3" />
             </div>
           </div>
-          <div className="w-1/2 pl-20 my-auto">
-            <div className="text-[#374048] text-4xl font-semibold mb-5">3. Get Score</div>
+          <div className="w-1/2 pl-3 lg:pl-20 my-auto">
+            <div className="text-[#374048] text-3xl lg:text-4xl font-semibold mb-5">
+              3. Get Score
+            </div>
             <div className="text-[#82898f] text-lg font-light mb-5">
               Discover how does you photo compare to the general population.
             </div>
-            <button className="bg-[#0084FF] hover:bg-[#006ACC] transition-colors ease-linear duration-300 text-[22px] leading-[65px] font-medium text-white rounded-[60px] px-[45px]">
+            <button className="bg-[#0084FF] hover:bg-[#006ACC] transition-colors ease-linear duration-300 max-[500px]:text-lg max-[500px]:py-4 max-[500px]:px-10 text-lg py-[18px] px-[34px] font-medium text-white rounded-[60px]">
               Find your rating
             </button>
           </div>
         </div>
       </section>
-      <section className="py-[111px] px-[70px] bg-[#F8FBFD]">
+      {/* <section className="py-[111px] px-[70px] bg-[#F8FBFD]">
         <div className="text-[48px] leading-[29px] text-[#374048] font-semibold text-center mb-[25px]">
           FEATURES
         </div>
