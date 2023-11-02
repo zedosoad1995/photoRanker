165f2e835eafb7674772dfdd8896eb51ef7029dc
João Queiroz <joao.pires.queiroz@gmail.com>
2023-11-02 23:02:26 +0000

    Update deploy scripts

--- infra/deploy/deploy.sh
+++ infra/deploy/deploy.sh
@@ -1,36 +1,33 @@
 #!/bin/bash
 
-cd app
-git pull origin master
+set -e # Exit on any error
 
-cd backend
-
-export NVM_DIR="/home/ubuntu/.nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
-
-if npx prisma migrate deploy; then
-    cd ~
-
-    # Login to Docker Hub
-    echo "$DOCKER_HUB_PASSWORD" | docker login --username "$DOCKER_HUB_USERNAME" --password-stdin
-
-    docker container prune -f
-    docker image prune -f
-
-    # Pull the latest images
-    docker pull vcarp/photoscorer:latest
-    docker pull vcarp/photoscorer-nginx:latest
-
-    # Use Docker Compose to restart the services
-    docker-compose down
-    docker-compose up -d
-
-    echo "Update complete."
-else
-    # Migration failed, handle the error as needed
-    # You can log the error, send notifications, or take other actions
-    echo "Database migration failed. Check logs for details."
-
-    # Optionally, you can exit the script to prevent further execution
+log_and_exit() {
+    echo "$1" >&2
     exit 1
+}
+
+echo "Starting deployment..."
+
+if [ "$1" == "migrate" ]; then
+    ./migrate.sh
 fi
+
+cd ~
+
+# Login to Docker Hub
+echo "$DOCKER_HUB_PASSWORD" | docker login --username "$DOCKER_HUB_USERNAME" --password-stdin
+
+# Clean current image/container to free up space
+docker container prune -f
+docker image prune -f
+
+# Pull the latest images
+docker pull vcarp/photoscorer:latest
+docker pull vcarp/photoscorer-nginx:latest
+
+# Use Docker Compose to restart the services
+docker-compose down
+docker-compose up -d
+
+echo "Update complete."
