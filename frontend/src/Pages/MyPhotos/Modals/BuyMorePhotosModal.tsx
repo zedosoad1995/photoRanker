7775e034a9978617df36dbc8e77e7f6e507232b7
João Queiroz <joao.pires.queiroz@gmail.com>
2023-11-19 18:58:48 +0000

    Add colurful icon to get many photos modal

--- frontend/src/Pages/MyPhotos/Modals/BuyMorePhotosModal.tsx
+++ frontend/src/Pages/MyPhotos/Modals/BuyMorePhotosModal.tsx
@@ -1,6 +1,6 @@
 import { Dialog } from "@headlessui/react";
 import Button from "@/Components/Button";
-import { PhotoIcon } from "@heroicons/react/20/solid";
+import { CameraRollIcon } from "@/Components/Icons/CameraRoll";
 
 interface IBuyMorePhotosModal {
   isOpen: boolean;
@@ -17,10 +17,10 @@ export default function BuyMorePhotosModal({ isOpen, onClose: handleClose }: IBu
     >
       <div className="fixed inset-0 bg-black/50 cursor-pointer" />
       <Dialog.Panel className="bg-white mx-2 p-6 w-[380px] rounded-xl z-30 max-h-[100vh] overflow-y-auto">
-        <div className="font-bold text-center text-lg">Get More Photos</div>
-        <div className="w-[85%] mx-auto text-center mt-4">
-          <div className="bg-yellow-200 rounded-full">
-            <PhotoIcon className="scale-75" />
+        <div className="font-bold text-center text-xl">Get More Photos</div>
+        <div className="w-[90%] mx-auto text-center mt-4">
+          <div className="m-auto w-[80%] rounded-full bg-[#fa8072] p-4">
+            <CameraRollIcon />
           </div>
           <div className="mt-4">Limit of 20 photos reached.</div>
           <div className="mb-8">
