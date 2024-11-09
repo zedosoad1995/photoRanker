169eb7468e892e86156e402b8fdbe62fa317919b
João Queiroz <joao.pires.queiroz@gmail.com>
2024-11-09 00:03:07 +0000

    Update scripts

--- infra/dockers/start_db.bat
+++
@@ -1,4 +0,0 @@
-docker-compose --file="infra/dockers/docker-compose-postgres.yml" -p photo_ranker up -d
-cd /d .\backend
-npx prisma migrate deploy
-npx prisma generate
--- infra/dockers/start_test_db.bat
+++
@@ -1,3 +0,0 @@
-docker-compose --file="infra/dockers/docker-compose-postgres-test.yml" -p photo_ranker_test  up -d
-cd /d .\backend
-npm run test:migrate
